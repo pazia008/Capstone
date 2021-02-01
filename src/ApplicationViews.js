71e370e4102e937592eb900b4319636b58f6f0e6
pazia008 <pazia008@gmail.com>
2021-02-01 14:34:07 -0600

    starting my home page

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { Home } from "./home/Home"
 import { TripForm } from "./trip/TripForm"
 import { TripProvider } from "./trip/TripProvider"
 import { UserProvider } from "./user/UserProvider"
@@ -9,6 +10,11 @@ import { UserProvider } from "./user/UserProvider"
 export const ApplicationViews = () => {
     return (
         <>
+
+            <Route exact path="/">
+                <Home />
+            </Route>
+
         <TripProvider>
             <UserProvider>
             <Route exact path="/tripform">
